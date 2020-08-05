6fe348c21729fd44a96ce0e267e3d36ac1d68cac
linaresb08 <linaresb08@gmail.com>
2020-08-05 17:32:30 -0400

    Intento de configuración del nuxt. Corregir errores de prettier.

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      bodyAttrs: {
+        class: 'font-sans bg-white',
+      },
+    },
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>hola soy index vue</div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Welcome',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Welcome',
+      },
+    ],
+  },
+}
+</script>
